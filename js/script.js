e1820eee8834eadc146b6c11b6718341bb216f37
Richard T <richard.tambong04@gmail.com>
2023-05-15 11:36:45 +0800

    update search

--- js/script.js
+++ js/script.js
@@ -11,6 +11,45 @@ const api_language = "&language=en-US"
 // https://api.themoviedb.org/3/movie/popular?api_key=a2cb6a5578c78a926883617fb6e2aedd + &page=1
 // https://api.themoviedb.org/3/tv/popular?api_key=a2cb6a5578c78a926883617fb6e2aedd + &page=1
 
+
+
+// ########################################
+// #             Now Playing              #
+// ########################################
+
+async function fetchNowPlayingMovies() {
+  try {
+    const now_playing_response = await fetch(api_Url + 'movie/now_playing' + api_Key + api_language);
+    const now_playing_data = await now_playing_response.json();
+    return now_playing_data.results;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+const NowPlayingMovies = async () => {
+  const now_playing_movies = await fetchNowPlayingMovies();
+  now_playing_movies.forEach((now_playing_movie) => {
+
+    const div = document.createElement("div");
+    div.classList.add("swiper-slide");
+    div.innerHTML = `
+      <a href="movie-details.html?id=${now_playing_movie.id}">
+        <img src="https://image.tmdb.org/t/p/w500${now_playing_movie.poster_path}" alt="${now_playing_movie.title}" />
+      </a>
+      <h4 class="swiper-rating">
+        <i class="fas fa-star text-secondary"></i> ${now_playing_movie.vote_average} / 10
+      </h4>
+    `;
+
+    document.querySelector(".swiper-wrapper").appendChild(div);
+
+    startSwiper();
+
+  });
+};
+
+
 // ########################################
 // #            POPULAR MOVIES            #
 // ########################################
@@ -53,7 +92,7 @@ const PopularMovies = async () => {
 
 
 // ########################################
-// #            TV Shows            #
+// #               TV Shows               #
 // ########################################
 
 async function fetchTV_Shows() {
@@ -88,47 +127,6 @@ const TV_Shows = async () => {
     });
 };
 
-
-
-
-// ########################################
-// #             Now Playing              #
-// ########################################
-
-async function fetchNowPlayingMovies() {
-  try {
-    const now_playing_response = await fetch(api_Url + 'movie/now_playing' + api_Key + api_language);
-    const now_playing_data = await now_playing_response.json();
-    return now_playing_data.results;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-const NowPlayingMovies = async () => {
-  const now_playing_movies = await fetchNowPlayingMovies();
-  now_playing_movies.forEach((now_playing_movie) => {
-
-    const div = document.createElement("div");
-    div.classList.add("swiper-slide");
-    div.innerHTML = `
-      <a href="movie-details.html?id=${now_playing_movie.id}">
-        <img src="https://image.tmdb.org/t/p/w500${now_playing_movie.poster_path}" alt="${now_playing_movie.title}" />
-      </a>
-      <h4 class="swiper-rating">
-        <i class="fas fa-star text-secondary"></i> ${now_playing_movie.vote_average} / 10
-      </h4>
-    `;
-
-    document.querySelector(".swiper-wrapper").appendChild(div);
-
-    startSwiper();
-
-  });
-};
-
-
-
 // ########################################
 // #             Search Movies            #
 // ########################################
@@ -148,19 +146,24 @@ function search(e){
     console.log(itemName.indexOf(text));
 
     if (itemName.indexOf(text) != -1) {
-      document.getElementById("popular-movies").classList = "card2"
-
+      item.parentElement.parentElement.style.display = 'block';
+      item.parentElement.parentElement.style.maxHeight = '503.25px';
+      item.parentElement.parentElement.style.maxWidth = '267.5px';
+      item.parentElement.parentElement.style.margin = 'auto';
+     
     } else {
-      item.parentElement.parentElement.style.display = 'none';
-
+      item.parentElement.parentElement.style.display = 'none'  
     }
   });
 }
+function init(){
+  itemSearch.addEventListener('input',  search); 
 
-// function init(){
-  itemSearch.addEventListener('input', search);  
-// }
-// init();
+}
+init();
+
+
+
 
 
 
@@ -168,91 +171,6 @@ function search(e){
 // ########################################
 // #            Movie Details             #
 // ########################################
-function fetchMovieDetail(){
-if (
-  window.location.href.indexOf('movie-details.html') > -1 ||
-  window.location.href === `/movie-details.html`
-) {
-  const params = new URLSearchParams(window.location.search);
-  const movieId = params.get('id');
-
-  // get movie details from TMDB API
-  const getMovieDetails = async () => {
-    const response = await fetch(
-      `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`
-    );
-    console.log(response);
-    const data = await response.json();
-    return data;
-  };
-
- // Overlay for background image
- displayBackgroundImage("tv", show.backdrop_path);
-  
- const div = document.createElement("div");
-
- div.innerHTML = `
- <div class="details-top">
- <div>
- ${
-   show.poster_path
-     ? `<img
-   src="https://image.tmdb.org/t/p/w500${show.poster_path}"
-   class="card-img-top"
-   alt="${show.name}"
- />`
-     : `<img
- src="../images/no-image.jpg"
- class="card-img-top"
- alt="${show.name}"
-/>`
- }
- </div>
- <div>
-   <h2>${show.name}</h2>
-   <p>
-     <i class="fas fa-star text-primary"></i>
-     ${show.vote_average.toFixed(1)} / 10
-   </p>
-   <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
-   <p>
-     ${show.overview}
-   </p>
-   <h5>Genres</h5>
-   <ul class="list-group">
-     ${show.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
-   </ul>
-   <a href="${
-     show.homepage
-   }" target="_blank" class="btn">Visit show Homepage</a>
- </div>
-</div>
-<div class="details-bottom">
- <h2>Show Info</h2>
- <ul>
-   <li><span class="text-secondary">Number of Episodes:</span> ${
-     show.number_of_episodes
-   }</li>
-   <li><span class="text-secondary">Last Episode To Air:</span> ${
-     show.last_episode_to_air.name
-   }</li>
-   <li><span class="text-secondary">Status:</span> ${show.status}</li>
- </ul>
- <h4>Production Companies</h4>
- <div class="list-group">
-   ${show.production_companies
-     .map((company) => `<span>${company.name}</span>`)
-     .join(", ")}
- </div>
-</div>
- `;
-
- document.querySelector("#show-details").appendChild(div);
-}
-
-
-}
-
 
 
 
@@ -261,10 +179,13 @@ if (
 // #          Calling Functions           #
 // ########################################
 
-// NowPlayingMovies();
-// PopularMovies();
-// TV_Shows();
+NowPlayingMovies();
+PopularMovies();
+TV_Shows();
 
+// ########################################
+// #             Swiper Area              #
+// ########################################
 
 function startSwiper() {
   const swiper = new Swiper(".swiper", {
